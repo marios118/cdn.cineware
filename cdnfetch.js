ad250d16df6579f6bd5e02ca00e28634e8a5a590
marios_118 <108134871+marios118@users.noreply.github.com>
2025-02-05 16:30:47 +0200

    Update cdnfetch.js

--- cdnfetch.js
+++ cdnfetch.js
@@ -1,26 +1,28 @@
 document.addEventListener('DOMContentLoaded', async function () {
-            const urlParams = new URLSearchParams(window.location.search);
-            const videoId = urlParams.get('video_id');
-            const seasonNumber = urlParams.get('s');
-            const episodeNumber = urlParams.get('e');
-            const loadingScreen = document.getElementById('loading-screen');
-            const videoContainer = document.getElementById('video-container');
-            const videoFrame = document.getElementById('video-frame');
+    const urlParams = new URLSearchParams(window.location.search);
+    const videoId = urlParams.get('video_id');
+    const seasonNumber = urlParams.get('s');
+    const episodeNumber = urlParams.get('e');
+    const loadingScreen = document.getElementById('loading-screen');
+    const videoContainer = document.getElementById('video-container');
+    const videoFrame = document.getElementById('video-frame');
 
-            if (videoId === '65334' && seasonNumber === '6' && episodeNumber === '2') {
-                videoFrame.src = "https://ghbrisk.com/e/g7kgtnz5sf98";
-            } else if (videoId && seasonNumber && episodeNumber) {
-                videoFrame.src = `https://multiembed.mov/directstream.php?video_id=${videoId}&tmdb=1&s=${seasonNumber}&e=${episodeNumber}`;
-            } else if (videoId) {
-                videoFrame.src = `https://multiembed.mov/directstream.php?video_id=${videoId}&tmdb=1`;
-            } else {
-                loadingScreen.innerText = 'Error: Video not found or invalid parameters.';
-                return;
-            }
+    if (videoId === '65334' && seasonNumber === '6' && episodeNumber === '2') {
+        videoFrame.src = "https://ghbrisk.com/e/g7kgtnz5sf98";
+    } else if (videoId === '65334' && seasonNumber === '6' && episodeNumber === '3') {
+        videoFrame.src = "https://ghbrisk.com/e/clzo3qnia18c";
+    } else if (videoId && seasonNumber && episodeNumber) {
+        videoFrame.src = `https://multiembed.mov/directstream.php?video_id=${videoId}&tmdb=1&s=${seasonNumber}&e=${episodeNumber}`;
+    } else if (videoId) {
+        videoFrame.src = `https://multiembed.mov/directstream.php?video_id=${videoId}&tmdb=1`;
+    } else {
+        loadingScreen.innerText = 'Error: Video not found or invalid parameters.';
+        return;
+    }
 
-            setTimeout(() => {
-                loadingScreen.style.display = "none";
-                videoContainer.style.display = "block";
-                videoFrame.style.pointerEvents = "auto";
-            }, 2000);
-        });
+    setTimeout(() => {
+        loadingScreen.style.display = "none";
+        videoContainer.style.display = "block";
+        videoFrame.style.pointerEvents = "auto";
+    }, 2000);
+});
